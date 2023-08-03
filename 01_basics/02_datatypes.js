96a91f67395554d8d68a232f0c6b4fe5db86f1d0
Shrutika Ekbote <130368572+ShrutikaEkbote@users.noreply.github.com>
2023-08-03 10:34:35 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,36 @@
+"use strict" //this command treat all JS code as newer version ,it uses all the syntax from new version of the JS.
+
+/* when we write here "alert" ,then nodejs didn't run that code ,until its defined in the console,
+but if we run this "alert" in the browsers console then it will run that code ,and gives the output.*/
+
+//alert( 3 + 3)
+
+// code readability should be high
+
+console.log( 3+3);    //output:"6"
+console.log("shrutika"); //output:"shrutika"
+
+let name="shrutika" // string 
+let age = 18 // number
+let isLoggedin = false;
+
+console.log(name);
+console.log(age);
+console.log(isLoggedin);
+
+
+// PRIMITIVE DATATYPES:
+
+//number => 2 to power 53
+//bigint =>when your number is too large 
+//string =""
+//boolean = true/false
+// null =>standalone value , represents null i.e. no value at all
+//undefined => represents undefined value
+
+
+//NON PRIMITIVE DATATYPES:
+
+//Arrays
+//Objects 
+//Regular Expressions
