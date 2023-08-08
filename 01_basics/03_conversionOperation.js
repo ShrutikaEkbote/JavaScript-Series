d56ce9227062bdd3b956392cda11c57307b0ccaf
Shrutika Ekbote <130368572+ShrutikaEkbote@users.noreply.github.com>
2023-08-08 13:24:23 +0530

    03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -75,3 +75,41 @@ let x = ++y // 11
 // prefix decrement
 let y = 10 // 9
 let x = --y // 9
+
+
+// 1 .Equal (==) and Strict Equal (===):
+//The equal operator (==) compares two values for equality. The strict equal operator (===) not only checks if the values are equal but 
+also ensures that their data types are the same.
+
+5 == '5';   // true (values are equal)
+5 === '5';  // false (values are equal, but data types are different)
+
+// 2.Not Equal (!=) and Strict Not Equal (!==):
+//The not equal operator (!=) checks if two values are not equal. The strict not equal operator (!==) checks for both inequality and 
+different data types.
+
+5 != '7';   // true (values are not equal)
+5 !== '5';  // true (values are equal, but data types are different)
+
+// Logical AND (&&):
+
+//The logical AND operator (&&) returns true if both operands are true. Otherwise, it returns false.
+
+true && true;   // true
+true && false;  // false
+
+// Logical OR (||):
+
+// The logical OR operator (||) returns true if at least one of the operands is true. It returns false if both operands are false.
+
+true || false;   // true
+false || false;  // false
+
+// Logical NOT (!):
+
+// The logical NOT operator (!) returns the opposite Boolean value of its operand.
+
+!true;   // false
+!false;  // true
+
+ 
